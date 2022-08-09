4e6ab27d220ffe5cc20360446e97527901ea51b8
ImDaeseong <cs93059@gmail.com>
2022-08-09 15:58:25 +0900

    Setup_test

---
+++ EzDefineDialog_test/ISUser2/ISUser1.cpp
@@ -0,0 +1,25 @@
+﻿#include "pch.h"
+#include "framework.h"
+#include "ISUser1.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+BEGIN_MESSAGE_MAP(CISUser1App, CWinApp)
+END_MESSAGE_MAP()
+
+CISUser1App::CISUser1App()
+{
+}
+
+CISUser1App theApp;
+
+/*
+BOOL CISUser1App::InitInstance()
+{
+	CWinApp::InitInstance();
+
+	return TRUE;
+}
+*/
---
+++ UseDLL_test/EzFun/EzFun.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+/*
+#ifndef __AFXWIN_H__
+	#error "PCH에 대해 이 파일을 포함하기 전에 'pch.h'를 포함합니다."
+#endif
+
+#include "resource.h"		// 주 기호입니다.
+
+class CEzFunApp : public CWinApp
+{
+public:
+	CEzFunApp();
+
+public:
+	//virtual BOOL InitInstance();
+	DECLARE_MESSAGE_MAP()
+};
+*/
+
+extern "C" _declspec(dllexport) bool ConnectWebPage(char* szUrl);
+extern "C" _declspec(dllexport) bool ShellExecuteUrl(char* szUrl);
